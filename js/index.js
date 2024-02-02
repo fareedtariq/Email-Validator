699ce9db00f9413ccbbda97905330b638be46757
Fareed <fareedtariq119@gmail.com>
2024-02-03 03:26:11 +0500

    second commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+console.log("This is my script")
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let key = "ema_live_BvVrNggOF7YQUON863irvvppbkNSHqO30CHFPP3h"
+    let email = document.getElementById("username").value 
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key]!== " "){ 
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML=str
+})
+
+
+
+
